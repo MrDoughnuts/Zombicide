a01be22987d99ab0e3e04ec0b31cbcb8197df637
EulaSimp <andyxu886@gmail.com>
2021-11-23 08:42:31 -0500

    added some basic framework for the menu

--- src/menu.java
+++ src/menu.java
@@ -1,8 +1,46 @@
 public class menu
 {
+    player _player = new player();
     public menu()
     {
 
     }
+
+    public void DisplayMenu()
+    {
+        DisplayInv();
+
+        System.out.println("Would you like to:");
+        System.out.println("1: move");
+        System.out.println("2: search");
+        System.out.println("3: attack");
+        System.out.println("____________________________");
+
+    }
+
+    public void DisplayInv()
+    {
+        System.out.println("____________________________");
+        System.out.println("Weapon: " + _player.DisplayWeapon());
+        System.out.println("Item: " + _player._item);
+        System.out.println("____________________________");
+    }
+
+    public void PlayerChoice(int choice)
+    {
+        If(choice == 1)
+        {
+            player.Move();
+        }
+        else if(choice == 2)
+        {
+            player.Search();
+        }
+        else if(choice == 3)
+        {
+            player.Attack();
+        }
+    }
+
 
 }
--- src/player.java
+++ src/player.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
+
 public class player
 {
+
+    public ArrayList<Integer> _weapon = new ArrayList<>();
+    public ArrayList<Integer> _item = new ArrayList<>();
+
+    public String[] _listOfWeapons = {"Frying Pan", "BaseBall Bat", "Fire Axe", "ChainSaw"};
     public player()
     {
-        
+
     }
+
+    public String DisplayWeapon()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            if(i == _weapon.get(0))
+            {
+                return _listOfWeapons[i];
+            }
+        }
+        return "";
+    }
+
+    public void SetWeapon(int __weapon)
+    {
+
+    }
+
+
 }
